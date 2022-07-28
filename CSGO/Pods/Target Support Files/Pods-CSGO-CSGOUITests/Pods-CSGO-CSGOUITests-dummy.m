594f73ac9b1dc946031e4a72d618bb06e5adbac2
danielbs2 <daniel.passos@bs2tecnologia.com.br>
2022-07-28 09:59:23 -0300

    add initial files and cocoa pods

---
+++ CSGO/Pods/Target Support Files/Pods-CSGO-CSGOUITests/Pods-CSGO-CSGOUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CSGO_CSGOUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CSGO_CSGOUITests
+@end
---
+++ CSGO/Pods/Target Support Files/Pods-CSGO/Pods-CSGO-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CSGO : NSObject
+@end
+@implementation PodsDummy_Pods_CSGO
+@end
